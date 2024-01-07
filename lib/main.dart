f23b0b4f2513f5230c3da61395e02050e3b1c0ac
Zack-19 <jz_safa@esi.dz>
2024-01-07 20:06:38 +0000

    Your commit message here

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// ignore: must_be_immutable
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text("app bar"),
+          backgroundColor: Colors.redAccent,
+          leading: Icon(Icons.menu),
+          elevation: 0,
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
+        ),
+        /*body: Center(
+          child: Container(
+            height: 250,
+            width: 250,
+            padding: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+                color: Colors.green, borderRadius: BorderRadius.circular(25)),
+            child: Text(
+              "Hello world",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),*/
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 150,
+              width: 150,
+              color: Colors.deepPurple,
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.deepPurple[400],
+            ),
+            Container(
+              height: 50,
+              width: 50,
+              color: Colors.deepPurple[200],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
